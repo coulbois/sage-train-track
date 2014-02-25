fe2e1faac9e3bbfdc5c354839118c235aa3a4824
Brian <brian@Picard.(none)>
2014-02-24 21:13:56 -0700

    added functions to top_rep

--- topological_representative.py
+++ topological_representative.py
@@ -12,6 +12,7 @@ from sage.rings.qqbar import AA
 from inverse_alphabet import AlphabetWithInverses
 from free_group import FreeGroup
 from free_group_automorphism import FreeGroupAutomorphism
+from sage.graphs.graph import DiGraph
 
 class TopologicalRepresentative(GraphMap):
     """
@@ -3428,3 +3429,117 @@ class TopologicalRepresentative(GraphMap):
 
 
         return result_morph
+
+    def gates(self,v):
+        '''
+        Returns a list of equivalence classes of edges out of a vertex v.
+        Author: Brian Mann
+        '''
+        if (v not in self.domain().vertices()):
+            v = self.domain().vertices()[0]
+            print "Not a valid vertex. Computing the gates at",v
+
+        directions = []
+        for e in self.domain().alphabet():
+            if self.domain().initial_vertex(e) == v:
+                directions.append(e)
+        for e in directions:
+            for f in directions:
+                if (e,f) in self.illegal_turns():
+                    directions.remove(f)
+        return directions
+
+    def number_of_gates(self,v):
+        '''
+        Returns the number of gates at v.
+        '''
+        return len(self.gates(v))
+
+    def local_whitehead_graph(self,v,verbose=True,show_plot=False):
+        '''
+        Returns the local whitegraph at a vertex v. Vertices are directions 
+        of v, and two directions are joined by an edge if some iterate of
+        and edge by self crosses that turn.
+
+        ASSUMES self IS FULLY IRREDUCIBLE!!!!!
+
+        Author: Brian Mann
+        '''
+        if not self.is_train_track():
+            print "You didn't input a train track. Applying train_track() for you."
+            self = self.train_track()
+        if (v not in self.domain().vertices()):
+            v = self.domain().vertices()[0]
+            print "Not a valid vertex. Picking vertex",v,"for you."
+            
+        directions = []
+        for e in self.domain().alphabet():
+            if self.domain().initial_vertex(e) == v:
+                directions.append(e)
+        for e in directions:
+            for f in directions:
+                if (e,f) in self.illegal_turns():
+                    directions.remove(f)
+
+        G = DiGraph([directions,lambda d1,d2: (d1,d2) in self.edge_turns()])
+        if verbose:
+            print G.to_undirected()
+        if show_plot:
+            G.to_undirected().plot()
+        return G.to_undirected()
+
+    def endpoints_of_pnp(self,path):
+        '''
+        Determines the fixed points of a periodic nielsen path, using the
+        format in the pnp function.
+
+        Author: Brian Mann
+        '''
+
+        p1 = self.domain().terminal_vertex(path[0][0][1])
+        p2 = self.domain().terminal_vertex(path[0][1][1])
+        if (p1 == p2):
+            return [p1]
+        else:    
+            return [p1,p2]
+
+    def nielsen_classes(self,verbose=True):
+        '''
+        For vertices v,w we define v~w if there exists a periodic nielsen path
+        from v to w. An equivlance class is called a nielsen class.
+
+        Returns a list of nielsen classes for self.
+
+        ASSUMES self IS FULLY IRREDUCIBLE!!!!!
+
+        Author: Brian Mann
+        '''
+        classes = []
+        for path in self.periodic_nielsen_paths():
+            pair = self.endpoints_of_pnp(path)
+            
+
+    def index_list(self,verbose=True):
+        '''
+        Computes Catherine Pfaff's index list. 
+        ASSUMES self IS FULLY IRREDUCIBLE!!!!!
+
+        Author: Brian Mann
+        '''
+        ind = []
+        if len(self.periodic_nielsen_paths()) == 0:
+            for v in self.domain.vertices():
+                ind.append(1-(self.number_of_gates(v))/2.0)
+                return ind
+        else:
+            return None
+    
+
+
+
+
+
+
+        
+
+
